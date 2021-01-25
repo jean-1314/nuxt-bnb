bf735ee1bb9d8883bdb59068388cfa6c3fdffc31
Ivan Savitsky <ivan@savitsky.biz>
2021-01-25 15:28:23 +0300

    Add tailwind

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,5 +20,7 @@ export default {
     MAPS_API_KEY: process.env.MAPS_API_KEY,
     ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID,
     ALGOLIA_API_KEY: process.env.ALGOLIA_API_KEY
-  }
+  },
+  modules: [],
+  buildModules: ['@nuxtjs/tailwindcss']
 }
