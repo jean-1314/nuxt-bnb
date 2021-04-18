a6bf6a434bec3b56d1e16fd8b386adf6ecc741f0
Ivan Savitsky <ivan@savitsky.biz>
2021-04-18 22:54:41 +0300

    Add add home page to admin section

--- modules/algolia/apis/index.js
+++ modules/algolia/apis/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 import userApi from './user';
+import homesApi from './homes';
 
 export default (algoliaConfig) => {
   return {
     user: userApi(algoliaConfig),
+    homes: homesApi(algoliaConfig),
   };
 }
--- plugins/maps.client.js
+++ plugins/maps.client.js
@@ -30,7 +30,7 @@ export default function(context, inject) {
     waiting = [];
   }
 
-  function makeAutoComplete(input) {
+  function makeAutoComplete(input, types = ['(cities)']) {
     if(!isLoaded){
       waiting.push({
         fn: makeAutoComplete,
@@ -39,9 +39,7 @@ export default function(context, inject) {
       return;
     }
 
-    const autoComplete = new window.google.maps.places.Autocomplete(input, {
-      types: ['(cities)']
-    });
+    const autoComplete = new window.google.maps.places.Autocomplete(input, { types });
     autoComplete.addListener('place_changed', () => {
       const place = autoComplete.getPlace();
       input.dispatchEvent(new CustomEvent('changed', { detail: place }));
